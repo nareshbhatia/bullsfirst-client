38e74f5086ee0763a7e20641785e046f07c1ef42
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-05-13 19:26:31 -0400

    Added a dummy SignIn page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Home, NotFound } from './pages';
+import { Home, NotFound, SignIn } from './pages';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignIn = () => {
+  return (
+    <div className="mt-2">
+      <h1 className="title">Sign In</h1>
+    </div>
+  );
+};
